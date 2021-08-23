ee57f95607c2d3246cd6758db6e499487a4ff862
hristo2612 <hristoapps@gmail.com>
2021-08-23 15:12:40 +0300

    03 - Good First Program

---
+++ 03-good-first-program/GoodFirstProgram.java
@@ -0,0 +1,13 @@
+
+public class GoodFirstProgram {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    System.out.println("We have a new line here!");
+    System.out.println("Good to know it's all working!");
+    System.out.println("Beginner level stuff here.");
+    System.out.println("We're getting there!");
+    System.out.println("We're almost there!");
+    System.out.println("End of prints, i am bored.");
+    System.out.println("You can escape double quotes like this: \"\"");
+  }
+}
